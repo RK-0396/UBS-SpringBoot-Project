db7f650a6e64254bf8c6cd660990183347b90cf6
RK-0396 <rk96.prajapati@gmail.com>
2024-04-22 23:02:43 +0530

    first commit

---
+++ src/main/java/com/example/UBSSpringBootProject/UbsSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.UBSSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UBSSpringBootProject/UbsSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UBSSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbsSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
